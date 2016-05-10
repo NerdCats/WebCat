d08173b168a105f14682b16ab84c8c46dcee6963
Swagata Prateek <s.prateek@hotmail.com>
2016-05-10 16:13:20 +0600

    Moving to Angular2 and TypeScript, boy this is going to be fun

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'webcat',
+  template: '<h1>Welcome to webcat</h1>'
+})
+
+export class AppComponent { }
---
+++ src/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'webcat',
+  template: '<h1>Welcome to webcat</h1>'
+})
+export class AppComponent { }
