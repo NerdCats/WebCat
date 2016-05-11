de5da1e0d438ba83526e7ccddd4d465b8ab3edee
Swagata Prateek <s.prateek@hotmail.com>
2016-05-11 17:42:53 +0600

    Folder restructure using LIFT

--- dist/app/app.component.js
+++ dist/app/app.component.js
@@ -22,5 +22,4 @@ var AppComponent = (function () {
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
-
 //# sourceMappingURL=app.component.js.map
--- systemjs.config.js
+++ systemjs.config.js
@@ -3,9 +3,9 @@
   // map tells the System loader where to look for things
   var map = {
     'app':                        'app', // 'dist',
-    'rxjs':                       '../node_modules/rxjs',
-    'angular2-in-memory-web-api': '../node_modules/angular2-in-memory-web-api',
-    '@angular':                   '../node_modules/@angular'
+    'rxjs':                       'node_modules/rxjs',
+    'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
+    '@angular':                   'node_modules/@angular'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
