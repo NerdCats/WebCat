02bd8c275645e440fbbbb784030ea369a8ac7f4e
Swagata Prateek <s.prateek@hotmail.com>
2016-05-11 19:09:27 +0600

    Added a AppSettings class just so we can add some settings that we'd like to use for WebCat

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { AppSettings } from './shared/app.config';
 
 @Component({
   selector: 'webcat',
-  template: '<h1>Welcome to webcat</h1>'
+  template: '<h1>Welcome to webcat</h1>',
+  providers: [Title]
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(title: Title) {
+    title.setTitle(AppSettings.APP_NAME);
+  }
+}
---
+++ app/shared/app.config.ts
@@ -0,0 +1,5 @@
+export class AppSettings {
+   public static get APP_NAME(): string {
+     return 'GO! Fetch';
+   }
+}
