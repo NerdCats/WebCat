3bfc9ceccd8dddffeccd43a01f4fa458ddc49e0e
Swagata Prateek <thehoneymad@hotmail.com>
2016-05-12 21:09:53 +0600

    Moved out the wrapHeader because we needed it

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,21 +3,20 @@ import { Title } from '@angular/platform-browser';
 import { CollapseDirective } from 'ng2-bootstrap/ng2-bootstrap';
 
 import { AppSettings } from './shared/app.config';
+import { WrapHeaderComponent } from './wrapHeader/WrapHeader.component';
 
 @Component({
   selector: 'webcat',
   templateUrl: 'app/app.component.html',
-  directives: [CollapseDirective],
+  directives: [CollapseDirective, WrapHeaderComponent],
   providers: [Title]
 })
 export class AppComponent {
-
-  public isCollapsed:boolean = false;
-  AppTitle : string;
+  public isCollapsed: boolean = false;
+  AppTitle: string;
 
   constructor(title: Title) {
     this.AppTitle = AppSettings.APP_NAME;
     title.setTitle(this.AppTitle);
   }
-
 }
---
+++ app/wrapHeader/wrapHeader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'wrap-header',
+    templateUrl: 'app/wrapHeader/wrapHeader.component.html',
+    styleUrls: ['app/wrapHeader/wrapHeader.component.css']
+})
+export class WrapHeaderComponent {
+
+}
