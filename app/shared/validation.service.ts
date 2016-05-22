9055dc78aad50045ae560dd0fda5c32b22f6c9ab
Swagata Prateek <thehoneymad@hotmail.com>
2016-05-23 02:08:16 +0600

    trying to put a validation service in place, should move these to account folder

---
+++ app/shared/validation.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+
+import { ValidationError } from './validationError';
+import { AccountService } from '../account/shared/account.service';
+
+@Injectable()
+export class ValidationService {
+
+    constructor(private accountService: AccountService) {
+
+    }
+
+    emailValidator(control): ValidationError {
+        if (control.value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)) {
+            return null;
+        } else {
+            return { 'invalidEmailAddress': true };
+        }
+    }
+
+    usernameValidatorAsync(control): Promise<ValidationError> {
+        return new Promise(resolve => {
+            this.accountService.checkUsername(control.value)
+                .subscribe(
+                result => {
+                    if (!result.IsAvailable) {
+                        resolve({ usernameTaken: true });
+                    }
+                    else {
+                        resolve(null);
+                    }
+                },
+                error => { resolve({ usernameTaken: false }) }
+                );
+        });
+    }
+}
