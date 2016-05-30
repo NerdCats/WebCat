82ceadf8f2b8c820f7db39bbba3ec81a062cd5ab
Swagata Prateek <s.prateek@hotmail.com>
2016-05-30 15:07:35 +0600

    Development build first iteration done, need to move to production build now

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,19 @@ const del = require('del');
 const typescript = require('gulp-typescript');
 const tscConfig = require('./tsconfig.json');
 const sourcemaps = require('gulp-sourcemaps');
+const tslint = require('gulp-tslint');
+
+// npm
+var install = require("gulp-install");
 
 // Cleanup the dist folder
 gulp.task('clean', function () {
-    return del('dist/**/*');
+    return del(['dist/**/*', '!dist/node_modules', '!dist/node_modules/**/*']);
+});
+
+// Hard cleanup the dist folder
+gulp.task('clean:hard', function () {
+    return del(['dist/**/*']);
 });
 
 
@@ -23,5 +32,37 @@ gulp.task('compile', ['clean'], function () {
         .pipe(gulp.dest('dist/app'));
 });
 
-gulp.task('build', ['compile']);
+// Tslint
+gulp.task('tslint', function() {
+  return gulp.src('app/**/*.ts')
+    .pipe(tslint())
+    .pipe(tslint.report('verbose'));
+});
+
+// copy dependencies from node_modules
+// we are just mimicking the dev environment now, but for production a lot more has to be done
+gulp.task('copy:libs', ['clean'], function () {
+    return gulp.src([
+        'node_modules/**/*'
+    ])
+        .pipe(gulp.dest('dist/node_modules'))
+});
+
+// copy static assets - i.e. non TypeScript compiled source
+gulp.task('copy:assets', ['clean'], function () {
+    return gulp.src(['app/**/*', 'assets/**/*', 'systemjs.config.js', 'config.js', 'package.json', 'index.html', 'styles.css', '!app/**/*.ts'], { base: './' })
+        .pipe(gulp.dest('dist'))
+});
+
+// install packages in dist, its still dev mode, we will have different folders for dev and prod mode later
+gulp.task('copy:dep', ['copy:assets'], function () {
+    gulp.src(['./dist/package.json'])
+        .pipe(install());
+});
+
+gulp.task('build', ['copy:dep', 'compile']);
 gulp.task('default', ['build']);
+
+
+
+
