b04a8bae49a71e631a2f643c20ef912ad160c570
Swagata Prateek <thehoneymad@hotmail.com>
2016-06-16 20:21:17 +0600

    Rx/Js import fix on LoginService

--- app/account/login/login.service.ts
+++ app/account/login/login.service.ts
@@ -3,6 +3,10 @@ import { Http, Headers, Response } from '@angular/http';
 import { AppSettings } from '../../shared/app.settings';
 import { Login } from './login';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
 import { LocalStorage } from '../../shared/local-storage';
 
 @Injectable()
