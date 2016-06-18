78c3d06481be5d2a4771f468e8b7c61fc34a73e0
Swagata Prateek <thehoneymad@hotmail.com>
2016-06-18 18:45:31 +0600

    #13, navbar simplified in Template

--- app/navbar/navbar.component.ts
+++ app/navbar/navbar.component.ts
@@ -11,6 +11,11 @@ import { Router } from '@angular/router-deprecated';
 
 type NavbarState = "PUBLIC" | "SECURED";
 
+interface NavbarElement {
+    Title: string;
+    Event(): void;
+}
+
 @Component({
     selector: 'navbar',
     templateUrl: 'app/navbar/navbar.component.html',
@@ -27,24 +32,38 @@ export class NavbarComponent {
 
     AppTitle: string;
     State: NavbarState = "PUBLIC";
-    UserNameString: string;
+
+    profileNavElement: NavbarElement;
+    userNameString: string;
+
+    private _publicNavElements: Array<NavbarElement>;
+    private _secureNavElements: Array<NavbarElement>;
+
+    navBarElementDict = {};
 
     isCollapsed: boolean;
 
     constructor(
         private localStorage: LocalStorage,
         private loginService: LoginService,
+        private cdr: ChangeDetectorRef,
         private router: Router) {
         this.AppTitle = AppSettings.APP_NAME;
 
         if (this.loginService.isLoggedIn) {
-            this.setToSecuredState();
+            this._setToSecuredState();
         }
+
+        console.log(this.loginService.isLoggedIn);
+        console.log(this.State);
+
+        this._initiatePublicNavElements();
+        this._initiateSecureNavElements();
     }
 
     onLoginCompleted(loginStatus: LoginStatus) {
         if (loginStatus == "SUCCESS") {
-            this.setToSecuredState();
+            this._setToSecuredState();
         }
     }
 
@@ -65,9 +84,48 @@ export class NavbarComponent {
         this.loginComponent.open();
     }
 
-    private setToSecuredState() {
+    private _initiatePublicNavElements() {
+        this._publicNavElements = new Array<NavbarElement>();
+        this._publicNavElements.push({
+            Title: "Sign Up",
+            Event: () => { this.showSignUpComponent(); }
+        });
+        this._publicNavElements.push({
+            Title: "Log In",
+            Event: () => { this.showLoginComponent(); }
+        });
+
+        this.navBarElementDict['PUBLIC'] = this._publicNavElements;
+    }
+
+    private _initiateSecureNavElements() {
+        this._secureNavElements = new Array<NavbarElement>();
+        this._secureNavElements.push({
+            Title: "Dashboard",
+            Event: () => { console.log("Not Implemented Yet"); }
+        });
+
+        this._secureNavElements.push({
+            Title: this.userNameString,
+            Event: () => { console.log("Not Implemented Yet"); }
+        });
+
+        this._secureNavElements.push({
+            Title: "Log Out",
+            Event: () => { this.logout(); }
+        });
+
+        this.navBarElementDict['SECURED'] = this._secureNavElements;
+    }
+
+    public _setToSecuredState() {
         this.State = "SECURED";
-        let authToken = this.localStorage.getObject("auth_token");
-        this.UserNameString = authToken["userName"];
+        var userName = this.localStorage.getObject("auth_token")["userName"];
+        this.userNameString = userName;
+
+        // INFO : This is literally shameless, but sometimes, you just gotta do it
+        if(this._secureNavElements) {
+            this._secureNavElements[1].Title = this.userNameString;
+        }
     }
 }
