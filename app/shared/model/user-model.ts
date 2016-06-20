02407071a99261ea6263dfa087efb6e32c3c8eed
Swagata Prateek <thehoneymad@hotmail.com>
2016-06-21 01:45:26 +0600

    #13, Adde user-model class to go on

--- app/shared/model/user-model.ts
+++ app/shared/model/user-model.ts
@@ -1,3 +1,13 @@
-export interface UserModel {
+export interface UserModelBase {
     UserId: string;
+    Type: string;
+    PhoneNumber: string;
+    Email: string;
+}
+
+export interface UserModel extends UserModelBase {
+    UserName: string;
+    PhoneNumberConfirmed: boolean;
+    EmailConfirmed: boolean;
+    Profile: Object;
 }
