c90217e70d2ec73fad2a69873d5ee25d15acd58b
Swagata Prateek <thehoneymad@hotmail.com>
2016-06-24 14:01:27 +0600

    #84, Started adding a child router for the dashboard as it would have all the dashboard component

--- app/dashboard/dashboard.component.ts
+++ app/dashboard/dashboard.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ROUTER_DIRECTIVES } from '@angular/router-deprecated';
+import { RouterOutlet, RouteConfig } from '@angular/router-deprecated';
+
 import { JobHistoryComponent } from '../job/job-history/job-history.component';
 import { JobSummaryComponent } from '../job/job-summary/job-summary.component';
+import { OrderComponent } from '../order/order.component';
 
 @Component({
     selector: 'dashboard',
     templateUrl: 'app/dashboard/dashboard.component.html',
-    directives: [ROUTER_DIRECTIVES, JobHistoryComponent, JobSummaryComponent]
+    directives: [RouterOutlet, JobHistoryComponent, JobSummaryComponent]
 })
+@RouteConfig([
+    { path: '/', name: 'order', component: OrderComponent, useAsDefault: true }
+])
 export class DashboardComponent implements OnInit {
     isSideBarOpen: boolean = true;
 
