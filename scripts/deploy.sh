e00c778a7ba1c4f6b0693b845d258c04598fcbf9
Swagata Prateek <thehoneymad@hotmail.com>
2016-06-25 12:15:38 +0600

    BUILDFIX: Added more travis changes, echoing build address and checking out master

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,11 +5,16 @@ git config user.name "webcatdeveldeploy"
 # $AZURE_REPO_URL needs to be set in your projects Variables section
 # and include both username and password, e.g: https://username:password@site.scm.azurewebsites.net:443/site.git
 
+echo "deploying to"
+echo ${AZURE_REPO_URL}
 # Clone Azure repository
-git clone ${AZURE_REPO_URL} ~/azure
+git clone ${AZURE_REPO_URL} ./azure
 
 # change into the local azure directory
-cd ~/azure
+cd ./azure
+
+git checkout master
+git pull origin master
 
 # delete local repository azure contents
 rm -rf *
