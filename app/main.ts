0de642b5ac8f7a0725ba8e68ab4472660f5f10ca
Swagata Prateek <thehoneymad@hotmail.com>
2016-06-25 11:08:18 +0600

    #22, fixes #22, used PathLocationStrategy so at least we can use this in production properly

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,6 @@
+import { provide } from '@angular/core';
 import { bootstrap } from '@angular/platform-browser-dynamic';
+import { LocationStrategy, Location, PathLocationStrategy } from '@angular/common';
 import { AppComponent } from './app.component';
 import { LOCAL_STORAGE_PROVIDERS } from './shared/local-storage';
 import { HTTP_PROVIDERS } from '@angular/http';
@@ -9,6 +11,7 @@ import 'rxjs/add/operator/catch';
 // INFO: Need to use a proper logging module here
 bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
+    provide(LocationStrategy, { useClass: PathLocationStrategy }),
     LOCAL_STORAGE_PROVIDERS,
     HTTP_PROVIDERS,
     SECURE_HTTP_PROVIDERS])
