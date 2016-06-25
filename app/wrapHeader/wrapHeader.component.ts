0b1b78ab535bb2c08900d0b12afd7fa396ca99eb
Muhammed Tareq Aziz <tareqaziz.aust.cse@gmail.com>
2016-06-25 16:23:51 +0600

    #14 from dashboard to job tracking page, on each job's HRID, a link is added

--- app/wrapHeader/wrapHeader.component.ts
+++ app/wrapHeader/wrapHeader.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { Router, ROUTER_DIRECTIVES } from '@angular/router-deprecated';
 @Component({
     selector: 'wrap-header',
     templateUrl: 'app/wrapHeader/wrapHeader.component.html',
-    styleUrls: ['app/wrapHeader/wrapHeader.component.css']
+    styleUrls: ['app/wrapHeader/wrapHeader.component.css'],
+    directives: [ROUTER_DIRECTIVES]
 })
 export class WrapHeaderComponent {
 
+    constructor(private router: Router){
+
+    }
+    gotoSearchpage(){
+        this.router.navigate(["Track", {jobId: "Job-U7GPB66G"}])
+    }
 }
