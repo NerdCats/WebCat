bc43fe4d85a855660e7f72622b4475b0ba8d3555
tareq89 <tareqaziz.aust.cse@gmail.com>
2016-07-20 11:44:10 +0600

    #96 tracking map show logic refactored

--- app/job/shared/coordinateInfo.ts
+++ app/job/shared/coordinateInfo.ts
@@ -6,13 +6,13 @@ export class CoordinateInfo{
     public assetLocationAvailable: boolean;
 
     constructor(job: Job){
-        if (job.Order.From.Point.coordinates != null) {
+        if (job.Order.From.Point.coordinates != null && job.Order.From.Point.coordinates.length !== 0) {
             this.pickUpAddressAvailable = true;
         } else{
             this.pickUpAddressAvailable = false;
         }
 
-        if (job.Order.To.Point.coordinates != null) {
+        if (job.Order.To.Point.coordinates != null && job.Order.From.Point.coordinates.length !== 0) {
             this.deliveryAddressAvailable = true;
         } else {
             this.deliveryAddressAvailable = false;
