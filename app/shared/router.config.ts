e4b4b6b8403b24c22b85f899bf0b7d3ee6822746
Fahad Wajed <fahadwajed@gmail.com>
2016-08-17 12:19:44 +0600

    adds front.component to router

--- app/shared/router.config.ts
+++ app/shared/router.config.ts
@@ -10,6 +10,7 @@ import { OrderComponent } from '../dashboard/order/order.component';
 
 import {JobHistoryComponent} from '../job/job-history/job-history.component';
 import {DashboardComponent} from '../dashboard/dashboard.component';
+import {FrontComponent} from '../front/front.component';
 
 export const Routes = [
     {
@@ -32,6 +33,11 @@ export const Routes = [
         path: '/dashboard/...',
         name: 'Dashboard',
         component: DashboardComponent
+    },
+    {
+        path: '/front/:frontId',
+        name: 'frontDetails',
+        component: FrontComponent
     }
 ]
 
