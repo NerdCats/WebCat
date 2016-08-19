bbf027aea2aa08628a357f2213e92329352dcc26
tareq89 <tareqaziz.aust.cse@gmail.com>
2016-08-19 12:51:42 +0600

    #115 added cart Modal

--- app/vendor-details/vendor-details.component.ts
+++ app/vendor-details/vendor-details.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ViewChild, OnInit } from "@angular/core";
 import { VendorDetailsService } from "./vendor-details.service";
+import { MODAL_DIRECTIVES, ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
 
 
 @Component({
     selector: 'vendor-details',
     templateUrl: 'app/vendor-details/vendor-details.component.html',
     styleUrls: ['app/vendor-details/vendor-details.component.css'],
+    directives: [MODAL_DIRECTIVES, ModalComponent],
     providers: [VendorDetailsService]
 })
 
@@ -20,6 +22,14 @@ export class VendorDetailsComponent implements OnInit {
 
     constructor(private vendorDetailsService: VendorDetailsService){
 
+    }
+    @ViewChild('cartModal')
+    cartModal: ModalComponent;
+    openCartModal(){
+        this.cartModal.open();
+    }
+    closeCartModal(){
+        this.cartModal.close();
     }
 
 }
