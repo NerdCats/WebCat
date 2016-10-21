52b1968e9d3786eea032ff047d16ba2b3d3ac3ad
tareq89 <tareqaziz.aust.cse@gmail.com>
2016-10-22 02:12:05 +0600

    GFETCH-165 added categories in front page search option

--- app/wrapHeader/wrapHeader.component.ts
+++ app/wrapHeader/wrapHeader.component.ts
@@ -2,28 +2,25 @@ import { Component } from '@angular/core';
 import { Router, ROUTER_DIRECTIVES } from '@angular/router-deprecated';
 import { FormBuilder, Validators, ControlGroup } from '@angular/common';
 import { TYPEAHEAD_DIRECTIVES } from 'ng2-bootstrap/components/typeahead';
+import { WrapHeaderService } from './wrapHeader.service';
 
 @Component({
     selector: 'wrap-header',
     templateUrl: 'app/wrapHeader/wrapHeader.component.html',
     styleUrls: ['app/wrapHeader/wrapHeader.component.css'],
-    directives: [ROUTER_DIRECTIVES, TYPEAHEAD_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES, TYPEAHEAD_DIRECTIVES],
+    providers: [WrapHeaderService]
 })
 
 export class WrapHeaderComponent {
     public selectedArea: string = "";
     public selectedItem: string;
 
-    public areas:string [] = ['Banani', 'Baridhara', 'Dhanmondi', 'Gulshan 1', 'Gulshan 2', 'Uttara'];
+    public areas:string [] = this.wrapHeaderService.WrapHeaderDetails.areas;
 
-    public items:string[] = [
-        'Coffee', 'Tea', 'Coca Cola', 'Pepsi', 'Fanta',
-        'Cake', 'Pastry', 'Almond', 'Rice', 'Flour',
-        'Chocolates', 'Flowers',
-        'Electronics', 'TV'
-    ];
+    public items:string[] = this.wrapHeaderService.WrapHeaderDetails.items;
 
-    constructor(private router: Router){}
+    constructor(private router: Router, private wrapHeaderService: WrapHeaderService){}
 
     goToVendorList(area:string){
         this.router.navigateByUrl("/vendors");
