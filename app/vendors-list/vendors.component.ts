68daa756bb10040d4a66115097aa023d294ef89f
tareq89 <tareqaziz.aust.cse@gmail.com>
2016-10-30 14:27:43 +0600

    resized vendor list item images

--- app/vendors-list/vendors.component.ts
+++ app/vendors-list/vendors.component.ts
@@ -29,6 +29,7 @@ export class VendorsComponent implements OnInit {
         this.vendorService.getVendors(this.area)
             .subscribe(res => {
                 this.vendors = res;
+                this.vendors.reverse();
 
                 this.status = "SUCCESSFUL";
 
@@ -42,42 +43,13 @@ export class VendorsComponent implements OnInit {
     }
 
     getTileClass(i) {
-        if (i % 3 == 0) {
+        if (i % 5 == 0) {
             // return 'big-module';
-            return "col-xs-12 col-md-12 col-sm-12 col-lg-12 big-module";
+            return "col-xs-8 col-md-8 col-sm-8 col-lg-8 big-module";
         }
         else {
             // return 'module';
-            return "col-xs-6 col-md-6 col-sm-6 col-lg-6 module";
+            return "col-xs-4 col-md-4 col-sm-4 col-lg-4 module";
         }
-    }
-
-    getImage(index: number){
-        let images = ['http://i.imgur.com/9Cfolu9.jpg',
-                        'http://imgur.com/DvmzsbV.jpg',
-                        'http://imgur.com/DvmzsbV.jpg',
-                        'http://i.imgur.com/iW1euxZ.jpg',
-                        'http://i.imgur.com/C1fbSZc.jpg',
-                        'http://i.imgur.com/1LGNGv8.jpg',
-                        'http://i.imgur.com/OrYcw8i.jpg',
-                        'http://i.imgur.com/5ex18sL.jpg',
-                        'http://i.imgur.com/DvmzsbV.jpg',
-                        'http://i.imgur.com/DvmzsbV.jpg',
-                        'http://i.imgur.com/DvmzsbV.jpg',
-                        'http://i.imgur.com/DvmzsbV.jpg'
-                    ]
-        if(index === 1) return images[11];
-        if(index%2 === 0) return images[0];
-        if(index%3 === 0) return images[1];
-        if(index%5 === 0) return images[2];
-        if(index%7 === 0) return images[3];
-        if(index%11 === 0) return images[4];
-        if(index%13 === 0) return images[5];
-        if(index%17 === 0) return images[6];
-        if(index%23 === 0) return images[7];
-        if(index%29 === 0) return images[8];
-        if(index%31 === 0) return images[9];
-        if(index%37 === 0) return images[10];
-
     }
 }
