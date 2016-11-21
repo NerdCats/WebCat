b9580b1c568903fda1b4a6e0a762380f2845106b
tareq89 <tareqaziz.aust.cse@gmail.com>
2016-11-21 13:47:37 +0600

    GFETCH-247 report generation working now

--- app/dashboard/report/report.component.ts
+++ app/dashboard/report/report.component.ts
@@ -6,6 +6,7 @@ import { ReportService } from './report.service';
 import { TimepickerComponent } from 'ng2-bootstrap/ng2-bootstrap';
 import {DATEPICKER_DIRECTIVES} from 'ng2-bootstrap/ng2-bootstrap';
 
+import { DashboardBusService } from '../dashboard-bus.service';
 @Component({
     selector: 'report',
     templateUrl: 'app/dashboard/report/report.component.html',
@@ -17,10 +18,35 @@ import {DATEPICKER_DIRECTIVES} from 'ng2-bootstrap/ng2-bootstrap';
 
 export class ReportComponent implements OnInit {
 
-    pickupTime: Date = new Date();
+    startHour: number = 0;
+    startMin: number = 0;
+    startSec: number = 0;
+    startTime: Date = new Date();
+
+    endHour: number = 23;
+    endMin: number = 59;
+    endSec: number = 59;
+    endTime: Date = new Date();
+
     ngOnInit(){
 
     }
+
+    constructor(private busService: DashboardBusService, private reportService: ReportService) {
+        this.busService.annouceSectionChange("Report");
+    }
+
+    generateReport(){
+        let _startTime = this.getIsoDate(this.startTime, this.startHour, this.startMin, this.startSec);
+        let _endTime = this.getIsoDate(this.endTime, this.endHour, this.endMin, this.endSec);
+
+        this.reportService.getReport(_startTime, _endTime);
+    }
+    getIsoDate(date, hour, min, sec) {
+        console.log(new Date(date.getFullYear(), date.getMonth(), date.getDate(), hour, min, sec));
+
+		return new Date(date.getFullYear(), date.getMonth(), date.getDate(), hour, min, sec).toISOString();
+	}
 
 
 }
--- app/dashboard/report/report.service.ts
+++ app/dashboard/report/report.service.ts
@@ -5,11 +5,24 @@ import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import { AppSettings } from '../../shared/app.settings';
+import { LocalStorage } from '../../shared/local-storage';
 
 @Injectable()
 export class ReportService {
 
-    constructor(private http: Http){
+    constructor(private http: Http, private localStorage: LocalStorage){
 
+    }
+
+    getReport(startTime: string, endTime: string){
+        let auth_token = JSON.parse(this.localStorage.get("auth_token"));
+        console.log(auth_token);
+        var url = AppSettings.SPYCAT_BASE + "/api/details?startdate="+startTime+
+                                            "&enddate="+endTime+"&usertype=ENTERPRISE&"+
+                                            "username=" + auth_token["userName"] +
+                                            "&generateexcel=true";
+        console.log(url);
+
+        window.open(url, "_blank")
     }
 }
